a39bece52e70d710170a844f86ac073ce72050d7
skolodny <spencerkolodny1@gmail.com>
2025-01-09 21:58:42 -0500

    added contextHolder

--- src/pages/PickSubmission.tsx
+++ src/pages/PickSubmission.tsx
@@ -20,17 +20,19 @@ const PickSubmission: React.FC = () => {
     const [messageApi, contextHolder] = message.useMessage();
 
     const success = () => {
-      messageApi.open({
-        type: 'success',
-        content: 'Saved successfully',
-      });
+        messageApi.open({
+            type: 'success',
+            content: 'Saved successfully',
+            duration: 10,
+        });
     };
-  
+
     const error = () => {
-      messageApi.open({
-        type: 'error',
-        content: 'Data failed to save. Ensure you are logged in',
-      });
+        messageApi.open({
+            type: 'error',
+            content: 'Data failed to save. Ensure you are logged in',
+            duration: 10,
+        });
     };
 
     useEffect(() => {
@@ -45,12 +47,12 @@ const PickSubmission: React.FC = () => {
             await axios
                 .get("http://localhost:5000/api/information/getInfo")
                 .then((res) => res.data)
-                .then((data: {information: { options: Array<Pick>}}) => setPickArray(data.information.options))
+                .then((data: { information: { options: Array<Pick> } }) => setPickArray(data.information.options))
                 .catch((err) => console.log(err));
         dataRes();
     }, []);
 
-    const handleChange = (index:number, value: number | string) => {
+    const handleChange = (index: number, value: number | string) => {
         // Create a copy of the array to avoid mutating state directly
         const newArray = [...currentChoices];
         newArray[index] = value;
@@ -59,38 +61,42 @@ const PickSubmission: React.FC = () => {
 
     const updateData = async () => {
         await axios
-        .get("http://localhost:5000/api/information/getInfo")
-        .then(success)
-        .catch(error);
+            .get("http://localhost:5000/api/information/getInfo")
+            .then(success)
+            .catch(error);
     }
-    
+
     return (
-        <div className={styles.container}>
-            {pickArray.map((element: Pick, index: number) => {
-                if (element.type === "text" || element.type === "number") {
-                    return (
-                        <div key={index} className={styles.inputGroup}>
-                            <label className={styles.label}>{element.question}</label>
-                            <input value={currentChoices?.[index]} className={styles.input} onChange={(e) => handleChange(index, e.target.value)} />
-                        </div>
-                    );
-                } else if (element.type === "radio") {
-                    return (
-                        <div key={index} className={styles.inputGroup}>
-                            <label className={styles.label}>{element.question}</label>
-                            <select value={currentChoices?.[index]} className={styles.radioGroup} onChange={(e) => handleChange(index, e.target.value)}>
-                                {element.options.map((option: string) => {
-                                    return (
-                                        <option value={option}>{option}</option>
-                                    );
-                                })}
-                            </select>
-                        </div>
-                    );
-                }
-            })}
-            <button onClick={() => updateData()}>Save</button>
-        </div>
+        <>
+            {contextHolder}
+            <div className={styles.container}>
+                {pickArray.map((element: Pick, index: number) => {
+                    if (element.type === "text" || element.type === "number") {
+                        return (
+                            <div key={index} className={styles.inputGroup}>
+                                <label className={styles.label}>{element.question}</label>
+                                <input value={currentChoices?.[index]} className={styles.input} onChange={(e) => handleChange(index, e.target.value)} />
+                            </div>
+                        );
+                    } else if (element.type === "radio") {
+                        return (
+                            <div key={index} className={styles.inputGroup}>
+                                <label className={styles.label}>{element.question}</label>
+                                <select value={currentChoices?.[index]} className={styles.radioGroup} onChange={(e) => handleChange(index, e.target.value)}>
+                                    {element.options.map((option: string) => {
+                                        return (
+                                            <option value={option}>{option}</option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                        );
+                    }
+                })}
+                <button onClick={() => updateData()}>Save</button>
+            </div>
+        </>
+
     );
 };
 
